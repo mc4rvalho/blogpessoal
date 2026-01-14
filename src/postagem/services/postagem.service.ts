a5756dcfad17018463ec46cd359903e385cd4674
mc4rvalho <mcarvalho093@gmail.com>
2026-01-14 17:18:52 -0300

    Criando CRUD da classe tema e integrando com a classe postagem

--- src/postagem/services/postagem.service.ts
+++ src/postagem/services/postagem.service.ts
@@ -1,6 +1,7 @@
 import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { DeleteResult, ILike, Repository } from 'typeorm';
+import { TemaService } from '../../tema/services/tema.service';
 import { Postagem } from '../entities/postagem.entity';
 
 @Injectable()
@@ -8,16 +9,24 @@ export class PostagemService {
   constructor(
     @InjectRepository(Postagem)
     private postagemRepository: Repository<Postagem>,
+    private temaService: TemaService,
   ) {}
 
   async findAll(): Promise<Postagem[]> {
-    return await this.postagemRepository.find();
+    return await this.postagemRepository.find({
+      relations: {
+        tema: true,
+      },
+    });
   }
 
   async findById(id: number): Promise<Postagem> {
     const postagem = await this.postagemRepository.findOne({
       where: {
         id,
+      },
+      relations: {
+        tema: true,
       },
     });
 
@@ -32,15 +41,21 @@ export class PostagemService {
       where: {
         titulo: ILike(`%${titulo}%`),
       },
+      relations: {
+        tema: true,
+      },
     });
   }
 
   async create(postagem: Postagem): Promise<Postagem> {
+    await this.temaService.findById(postagem.tema.id);
     return await this.postagemRepository.save(postagem);
   }
 
   async update(postagem: Postagem): Promise<Postagem> {
     await this.findById(postagem.id);
+
+    await this.temaService.findById(postagem.tema.id);
 
     return await this.postagemRepository.save(postagem);
   }
---
+++ src/tema/controllers/tema.controller.ts
@@ -0,0 +1,55 @@
+import {
+  Controller,
+  Get,
+  HttpCode,
+  HttpStatus,
+  Param,
+  ParseIntPipe,
+  Post,
+  Body,
+  Put,
+  Delete,
+} from '@nestjs/common';
+import { Tema } from '../entities/tema.entity';
+import { TemaService } from '../services/tema.service';
+
+@Controller('/temas')
+export class TemaController {
+  constructor(private readonly temaService: TemaService) {}
+
+  @Get()
+  @HttpCode(HttpStatus.OK)
+  findAll(): Promise<Tema[]> {
+    return this.temaService.findAll();
+  }
+
+  @Get('/:id')
+  @HttpCode(HttpStatus.OK)
+  findById(@Param('id', ParseIntPipe) id: number): Promise<Tema> {
+    return this.temaService.findById(id);
+  }
+
+  @Get('/descricao/:descricao')
+  @HttpCode(HttpStatus.OK)
+  findByAllTitulo(@Param('descricao') descricao: string): Promise<Tema[]> {
+    return this.temaService.findAllByDescricao(descricao);
+  }
+
+  @Post()
+  @HttpCode(HttpStatus.CREATED)
+  create(@Body() tema: Tema): Promise<Tema> {
+    return this.temaService.create(tema);
+  }
+
+  @Put()
+  @HttpCode(HttpStatus.OK)
+  update(@Body() tema: Tema): Promise<Tema> {
+    return this.temaService.update(tema);
+  }
+
+  @Delete('/:id')
+  @HttpCode(HttpStatus.NO_CONTENT)
+  delete(@Param('id', ParseIntPipe) id: number) {
+    return this.temaService.delete(id);
+  }
+}
